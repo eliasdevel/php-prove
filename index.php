7101729eb66a3f2fc7ce100870c28d3bf1d6b2d0
Elias Müller <elias.muller1@gmail.com>
2020-11-29 07:46:57 -0300

    project

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Classmap\Number as Number;
 
-
-$i = 100;
-// iterate from 0 to N 
-for ($num = 0; $num < $i; $num++) {
+for ($num = 1; $num <= 100; $num++) {
     $number = new Number($num);
     $number->print();  
 }
